3f54e2e5406e40336dce379fb5d1f356d6dc964d
Yazan Baker <yazanbaker94@hotmail.com>
2021-09-30 13:58:26 +0300

    done

--- src/auth/models/mechanic.model.js
+++ src/auth/models/mechanic.model.js
@@ -3,9 +3,11 @@
 const mechanicModel = (sequelize, DataTypes) => sequelize.define('mechanic', {
   name: { type: DataTypes.STRING, required: true },
   category: { type: DataTypes.STRING, required: true },
-  description: { type: DataTypes.STRING, required: true },
+  description: { type: DataTypes.STRING },
   inStock: { type: DataTypes.INTEGER},
-  image: { type: DataTypes.STRING, required: true },
+  image: { type: DataTypes.STRING},
+  phoneNumber: { type: DataTypes.STRING, required: true },
+  price: { type: DataTypes.STRING},
 
 });
 
